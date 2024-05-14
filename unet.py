5cdf0107a6241bfaa19ec17a39b095d949459174
H4wk <quasand89@gmail.com>
2024-05-14 15:35:55 +0200

    add monai

--- unet.py
+++ unet.py
@@ -1,101 +1,98 @@
 from torch import cat, nn
+from torch.nn import functional as F
 
 
+# TODO: this shit is too slow find out why
 class Unet(nn.Module):
     def __init__(self, input_channel=1):
         super().__init__()
-        self.encoder_1 = Encoder(input_channel, 64)
-        self.encoder_2 = Encoder(64, 128)
-        self.encoder_3 = Encoder(128, 256)
-        self.encoder_4 = Encoder(256, 512)
+        self.inc = DoubleConv(input_channel, 64)
 
-        self.conv_block = ConvBlock(512, 1024)
+        self.encoder_1 = Encoder(64, 128)
+        self.encoder_2 = Encoder(128, 256)
+        self.encoder_3 = Encoder(256, 512)
+        self.encoder_4 = Encoder(512, 1024)
 
         self.decoder_1 = Decoder(1024, 512)
         self.decoder_2 = Decoder(512, 256)
         self.decoder_3 = Decoder(256, 128)
         self.decoder_4 = Decoder(128, 64)
 
-        self.cls = nn.Conv2d(64, 1, kernel_size=1, padding=0)
-        self.act = nn.Sigmoid()
+        self.outc = OutConv(64, 1)
 
     def forward(self, x):
-        """------ Encoder ------"""
-        x1, p1 = self.encoder_1(x)
-        x2, p2 = self.encoder_2(p1)
-        x3, p3 = self.encoder_3(p2)
-        x4, p4 = self.encoder_4(p3)
+        x1 = self.inc(x)
 
-        """ ------ BottleNeck ------"""
-        x5 = self.conv_block(p4)
+        x2 = self.encoder_1(x1)
+        x3 = self.encoder_2(x2)
+        x4 = self.encoder_3(x3)
+        x5 = self.encoder_4(x4)
 
-        """ ------ Decoder ------"""
-        x6 = self.decoder_1(x5, x4)
-        x7 = self.decoder_2(x6, x3)
-        x8 = self.decoder_3(x7, x2)
-        x9 = self.decoder_4(x8, x1)
+        x = self.decoder_1(x5, x4)
+        x = self.decoder_2(x, x3)
+        x = self.decoder_3(x, x2)
+        x = self.decoder_4(x, x1)
 
-        """ ------ Final Layer ------"""
-        x_final = self.cls(x9)
-        x_final = self.act(x_final)
+        logits = self.outc(x)
+        return logits
 
-        return x_final
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+    
+    def forward(self, x):
+        return self.conv(x)
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
 
 class Encoder(nn.Module):
-    def __init__(self, input_channel, out_channel, dropout=0.5):
-        super(Encoder, self).__init__()
-        self.conv2d_1 = ConvBlock(input_channel, out_channel, dropout)
-        self.maxpool = nn.MaxPool2d((2, 2))
-        self.dropout = nn.Dropout(dropout)
+    def __init__(self, input_channel, out_channel):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(input_channel, out_channel)
+        )
 
     def forward(self, x):
-        x = self.conv2d_1(x)
-        p = self.maxpool(x)
-        p = self.dropout(p)
-
-        return x, p
-
-class ConvBlock(nn.Module):
-    def __init__(self, input_channel, out_channel, dropout=0.5):
-        super(ConvBlock, self).__init__()
-        self.conv2d_1 = nn.Conv2d(input_channel, out_channel, kernel_size=3, padding=1)
-        self.batchnorm_1 = nn.BatchNorm2d(out_channel)
-        self.act = nn.ReLU()
-
-        self.dropout = nn.Dropout(dropout)
-
-        self.conv2d_2 = nn.Conv2d(out_channel, out_channel, kernel_size=3, padding=1)
-        self.batchnorm_2 = nn.BatchNorm2d(out_channel)
-
-    def forward(self, x):
-        x = self.conv2d_1(x)
-        x = self.batchnorm_1(x)
-        x = self.act(x)
-
-        x = self.dropout(x)
-
-        x = self.conv2d_2(x)
-        x = self.batchnorm_2(x)
-        x = self.act(x)
-
-        return x
-
+        return self.maxpool_conv(x)
 
 class Decoder(nn.Module):
-    def __init__(self, input_channel, output_channel, dropout=0.5):
-        super(Decoder, self).__init__()
+    def __init__(self, input_channel, output_channel):
+        super().__init__()
         self.conv_t = nn.ConvTranspose2d(
             input_channel, output_channel, stride=2, kernel_size=2
         )
-        self.conv2d_1 = ConvBlock(output_channel * 2, output_channel, dropout)
-        self.dropout = nn.Dropout(dropout)
+        self.conv2d_1 = DoubleConv(input_channel, output_channel)
 
-    def forward(self, x, skip):
-        x = self.conv_t(x)
-        x = cat([x, skip], dim=1)
-        x = self.dropout(x)
-        x = self.conv2d_1(x)
+    def forward(self, x1, x2):
+        x1 = self.conv_t(x1)
 
-        return x
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+
+        x = cat([x2, x1], dim=1)
+        return self.conv2d_1(x)
 
 
